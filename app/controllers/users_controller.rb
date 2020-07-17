5736f83fc0696b79806cf9e7d94a56394e5a665a
n0808 <life.with.chums.119@gmail.com>
2020-07-17 18:02:28 +0900

    Create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   
   def index
+    #キーワードがなければnilを返す
+    return nil if params[:keyword] == ""
+    #その文字を含むユーザーを探してきて、@usersに代入、ただしログインしている自分は除く。10人まで。
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
     respond_to do |format|
       format.html
       format.json
@@ -23,4 +27,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
+
 end
